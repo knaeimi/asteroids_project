c27f6c8f58a7f4968f2fedef6e1563ba3d7bf068
sean-sweeney25 <sean@bepo.co>
2025-04-18 10:19:11 -0500

    RocketShipCreated

--- src/AsteroidsGame.java
+++ src/AsteroidsGame.java
@@ -1,5 +1,19 @@
 import edu.macalester.graphics.*;
 
 public class AsteroidsGame {
+    private static final int CANVAS_WIDTH = 600;
+    private static final int CANVAS_HEIGHT = 600;
+    private RocketShip rocketShip;
+    private CanvasWindow canvas;
+
+    public AsteroidsGame(){
+        canvas = new CanvasWindow("Asteroids", CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+
+    public static void main(String[] args) {
+        new AsteroidsGame();
+        
+    }
+
 
 }
--- src/RocketShip.java
+++ src/RocketShip.java
@@ -1,5 +1,45 @@
 import edu.macalester.graphics.*;
 
-public class RocketShip {
 
+public class RocketShip {
+    private double centerX;
+    private double centerY;
+    private static final double SIDE_LENGTH = 5;
+    private Path rocketShape;
+    private double x0, y0;
+
+    public RocketShip (double centerX, double centerY, double x0, double y0){
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.x0 = x0;
+        this.y0 = y0;
+
+        rocketShape = Path.makeTriangle(x0, y0, x0 + SIDE_LENGTH, y0, x0 + (SIDE_LENGTH/2), y0 + SIDE_LENGTH);
+    }
+
+    public void setCenter(double newX, double newY){
+        this.centerX = newX;
+        this.centerY = newY;
+        rocketShape.setCenter(newX, newY);
+    }
+
+    public double getCenterX(){
+        return centerX;
+    }
+
+    public double getCenterY(){
+        return centerY;
+    }
+
+    public void addToCanvas(CanvasWindow canvas){
+        canvas.add(rocketShape);
+    }
+
+    public void removeFromCanvas(CanvasWindow canvas){
+        canvas.remove(rocketShape);
+    }
+
+    public GraphicsObject getShape(){
+        return this.rocketShape;
+    }
 }
