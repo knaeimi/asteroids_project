33633e2082b38cc6f1d977f453f324cd748370b9
sean-sweeney25 <sean@bepo.co>
2025-04-23 10:23:17 -0500

    GetCenter methods added to Projectile

--- src/Projectile.java
+++ src/Projectile.java
@@ -3,4 +3,6 @@ public interface Projectile {
     public boolean updatePosition();
     public void addToCanvas();
     public void removeFromCanvas();
+    public double getCenterX();
+    public double getCenterY();
 }
